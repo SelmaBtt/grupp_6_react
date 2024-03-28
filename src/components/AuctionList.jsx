2ac649395cb9209d5e3f5fe1e0ba8c8a4ddbb5c3
Selma Brattli <selma@brattli.nu>
2024-03-28 10:21:37 +0100

    Kod kommentarer för lösning

--- src/components/AuctionList.jsx
+++ src/components/AuctionList.jsx
@@ -1,22 +1,25 @@
-const AuctionList = (props) => {
-    const data = props.data;
+const AuctionList = (Auctions) => {
+    const data = Auctions; // Behöver inte ha data som värde, du kan skriva direkt på rad 7: Auctions && Auctions.length > 0, Auctions.map
+    // console.log(data)
 
     return (
-        {
-            data.map((item) => (
+        <>
+            {
+                (data && data.length > 0) && data.map((item) => ( // Lägga till conditional rendering så att den vet när den ska mappa tror jag 
 
-                <div>
-                    <h2>{item.AuctionID}</h2>
-                    <h3>{item.Description}</h3>
-                    <h3>{item.StartDate}</h3>
-                    <h4>{item.EndDate}</h4>
-                    <h4>{item.GroupCode}</h4>
-                    <h4>{item.StartingPrice}</h4>
-                    <h4>{item.CreatedBy}</h4>
+                    <div>
+                        <h2>{item.AuctionID}</h2>
+                        <h3>{item.Description}</h3>
+                        <h3>{item.StartDate}</h3>
+                        <h4>{item.EndDate}</h4>
+                        <h4>{item.GroupCode}</h4>
+                        <h4>{item.StartingPrice}</h4>
+                        <h4>{item.CreatedBy}</h4>
 
-                </div>
-            ))
-        }
+                    </div>
+                ))
+            }
+        </>
 
 
     );
