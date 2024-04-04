54745dee841a9d6c2764068452f346cf941a8066
Anna Olsson <annapolsson@hotmail.com>
2024-04-04 09:58:56 +0200

    laddade ned bootstrap, la till länk på title

--- src/components/FetchAll.jsx
+++ src/components/FetchAll.jsx
@@ -1,39 +1,41 @@
 import { useState, useEffect } from "react";
-import AuctionList from "./AuctionList";
+import { Link } from 'react-router-dom';
+import Auction from "./Auction";
 
 const FetchAll = () => {
-    const[data, setData] = useState([])
+    const [data, setData] = useState([])
 
     useEffect(() => {
         fetch('https://auctioneer.azurewebsites.net/auction/6fed')
-        .then(response => {
-            if(!response.ok){
-                throw Error (console.log('Error: ' + error))
-            }
-            return response.json();
-        })
-        .then(result => setData(result))
-        .catch(error => console.log('Error: ' + error))
+            .then(response => {
+                if (!response.ok) {
+                    throw Error(console.log('Error: ' + error))
+                }
+                return response.json();
+            })
+            .then(result => setData(result))
+            .catch(error => console.log('Error: ' + error))
     }, [])
 
-    return(
+    return (
         <>
             {
                 data && data.map((item) => ( // Lägga till conditional rendering så att den vet när den ska mappa tror jag 
 
                     <div>
-                       <h3>{item.Title}</h3>
+                        <Link to={`/Auction`}>{item.Title}</Link>
                         <h3>{item.Description}</h3>
                         <h3>{item.StartDate}</h3>
                         <h4>{item.EndDate}</h4>
                         <h4>{item.StartingPrice}</h4>
                         <h4>{item.CreatedBy}</h4>
-
                     </div>
                 ))
             }
         </>
     )
 }
+
+
 
 export default FetchAll;
