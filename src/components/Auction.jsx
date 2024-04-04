583c5fea922d82d95f513c919ca9e4e51d1c9340
Selma Brattli <selma@brattli.nu>
2024-04-04 14:12:02 +0200

    Router feature completed

--- src/components/Auction.jsx
+++ src/components/Auction.jsx
@@ -1,10 +1,25 @@
-import { useParams } from "react-router-dom";
+import { useParams, useLocation } from "react-router-dom";
 
 const Auction = () => {
-  const { id } = useParams()
+
+    const { id } = useParams()
+
+    const location = useLocation();
+    const { item } = location.state || {};
     return (
       <>
         <h1>HEj: {id} </h1>
+
+        {item && ( // Render item if it exists
+          <div>
+            <h2>{item.Title}</h2>
+            <h3>{item.Description}</h3>
+            <h3>{item.StartDate}</h3>
+            <h4>{item.EndDate}</h4>
+            <h4>{item.StartingPrice}</h4>
+            <h4>{item.CreatedBy}</h4>
+          </div>
+        )}
       </>
     )
 };
